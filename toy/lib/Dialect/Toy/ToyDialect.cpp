3d15621783a671291f92982f68cd22c7bdddde33
Frederik Madsen <frederik.meisner@gmail.com>
2025-02-18 12:35:31 +0100

    Register new type

--- toy/lib/Dialect/Toy/ToyDialect.cpp
+++ toy/lib/Dialect/Toy/ToyDialect.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 
 #include "toy/Dialect/Toy/ToyDialect.h"
+#include "toy/Dialect/Toy/ToyTypes.h"
+#include "mlir/IR/Builders.h"
+#include "llvm/ADT/TypeSwitch.h"
+
 #include "toy/Dialect/Toy/ToyDialect.cpp.inc"
+#define GET_TYPEDEF_CLASSES
+#include "toy/Dialect/Toy/ToyTypes.cpp.inc"
 
 namespace mlir::toy
 {
     void ToyDialect::initialize()
     {
+        addTypes<
+#define GET_TYPEDEF_LIST
+#include "toy/Dialect/Toy/ToyTypes.cpp.inc"
+            >();
+
         std::cout << "We have initialized the toy dialect" << std::endl;
     }
 }
