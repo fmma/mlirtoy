604ea8b90317a2ea80102ea74a2790ad629083ec
Frederik Madsen <frederik.meisner@gmail.com>
2025-02-18 12:17:58 +0100

    Added a simple type to the toy dialect

---
+++ toy/include/toy/Dialect/Toy/ToyTypes.h
@@ -0,0 +1,9 @@
+#ifndef MLIRTOY_TOY_INCLUDE_TOY_DIALECT_TOY_TOYTYPES_H_
+#define MLIRTOY_TOY_INCLUDE_TOY_DIALECT_TOY_TOYTYPES_H_
+
+#include "mlir/IR/DialectImplementation.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "toy/Dialect/Toy/ToyTypes.h.inc"
+
+#endif
