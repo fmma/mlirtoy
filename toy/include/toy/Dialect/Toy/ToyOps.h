7a24ed4f6f71f1396fe888bb02119b4663b330f6
Frederik Madsen <frederik.meisner@gmail.com>
2025-02-20 12:57:30 +0100

    More ops

--- toy/include/toy/Dialect/Toy/ToyOps.h
+++ toy/include/toy/Dialect/Toy/ToyOps.h
@@ -2,6 +2,8 @@
 #define MLIRTOY_TOY_INCLUDE_TOY_DIALECT_TOY_TOYOPS_H_
 
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
 
 #define GET_OP_CLASSES
 #include "toy/Dialect/Toy/ToyOps.h.inc"
